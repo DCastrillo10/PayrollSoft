377ffcd9936ab70a1121da7b14bc0a49c776ac3f
Deimer Castrillo <dcastrillomartinez@gmail.com>
2023-02-26 13:35:02 -0500

    Crear Proyecto

---
+++ PayrollSoft.ApplicationWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PayrollSoft.ApplicationWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
